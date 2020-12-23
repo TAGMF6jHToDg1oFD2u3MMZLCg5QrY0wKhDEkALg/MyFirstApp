67535437af800813f22d824ca3f747aae84574c0
Mike Martin <chachiisgod@protonmail.com>
2020-12-23 15:52:00 -0500

    First commit

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.brown
     }
-
-
+    @IBAction func showMessage(sender: UIButton)
+    {
+        let emojiDict: [String: String] = ["🤡": "Clown"]
+        var meaning: String?
+        
+        // The sender is the button that is tapped by the user.
+        // Here we store the sender in the selectedButton constant
+        let selectedButton = sender
+        
+        // Get the emoji from the title label of the selected button
+        if let wordToLookup = selectedButton.titleLabel?.text
+        {
+            meaning = emojiDict[wordToLookup]
+        }
+        let alertController = UIAlertController(title: "Meaning", message: meaning, preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
 }
 
